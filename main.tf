1421548223f2444ce002946e9be4d3c413622248
shahab-github <shahabuddinm679+github@gmail.com>
2023-08-16 21:55:53 -0500

    added subnets to main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_vpc" "this" {
     Name        = "${var.name}"
   }
 }
+
+
+resource "aws_subnet" "public" {
+  count = length(var.public_cidr)
+  vpc_id = aws_vpc.this.id
+  cidr_block = var.public_cidr[count.index]
+  availability_zone = var.availability_zone[count.index]
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,11 @@ variable "name" {
 variable "cidr" {
   default = "192.168.0.0/16"
 }
+
+variable "availability_zone" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "public_cidr" {
+  default = ["192.168.1.0/24", "192.168.2.0/24"]
+}
